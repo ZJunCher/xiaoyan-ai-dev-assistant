c08ae8e8d6311ba0c45aa179658aaee1ac4c22db
zhaojun <111044543+ZJunCher@users.noreply.github.com>
2026-05-12 10:28:45 +0800

    Upload src/test/java/com/xiaoyan/aiassistant/XiaoYanAiAssistantApplicationTests.java

---
+++ src/test/java/com/xiaoyan/aiassistant/XiaoYanAiAssistantApplicationTests.java
@@ -0,0 +1,12 @@
+package com.xiaoyan.aiassistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XiaoYanAiAssistantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
