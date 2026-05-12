c8935a5e6670c08eb451b962930dafe5533c28da
zhaojun <111044543+ZJunCher@users.noreply.github.com>
2026-05-12 10:28:25 +0800

    Upload src/main/java/com/xiaoyan/aiassistant/XiaoYanAiAssistantApplication.java

---
+++ src/main/java/com/xiaoyan/aiassistant/XiaoYanAiAssistantApplication.java
@@ -0,0 +1,14 @@
+package com.xiaoyan.aiassistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XiaoYanAiAssistantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XiaoYanAiAssistantApplication.class, args);
+    }
+
+}
+
